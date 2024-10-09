524e16a39b107f67584dc1c30fc8fa97b89e05c7
cotox3d <165819989+cotox3d@users.noreply.github.com>
2024-10-09 13:35:43 +0000

    interaccion1

---
+++ mentor/src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl:'./login.component.html',
+  styleUrls: ['./login.component.css'],
+  imports: [CommonModule, FormsModule]
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string | null = null;
+
+  constructor(private router: Router) {}
+
+ onLogin() {
+   const validUsername = 'admin';
+   const validPassword = '123';
+   const validMemberUsername = 'ALUMNO';
+   const validMemberPassword = '123';
+
+    if (this.username === validUsername && this.password === validPassword) {
+      this.errorMessage = null;
+      alert('Login exitoso');
+      this.router.navigate(['/principal']);
+    }
+    else if (this.username === validMemberUsername && this.password === validMemberPassword) {
+      this.errorMessage = null;
+      alert('Login exitoso');
+      this.router.navigate(['/principal']);
+    } else {
+      alert('incorrecto');
+    }
+  }
+}
